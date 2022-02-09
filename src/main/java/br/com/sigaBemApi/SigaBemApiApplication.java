c416d234423ba778ba786d0172815b4479db0873
Potier Marques <potier.marques@gmail.com>
2022-02-08 22:00:55 -0300

    #PROJETO BASE#

---
+++ src/main/java/br/com/sigaBemApi/SigaBemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sigaBemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaBemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaBemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sigaBemApi/SigaBemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sigaBemApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaBemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
